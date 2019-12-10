32564e24a5e14f2b35271426cdd546006c3761fc
Jerome Leclanche <jerome@leclan.ch>
2019-12-10 17:38:31 +0100

    Fix form header not to scroll

--- src/components/RsvpForm.tsx
+++ src/components/RsvpForm.tsx
@@ -46,83 +46,87 @@ export default () => {
             Request an RSVP
           </button>
 
-          <Form className={isActive ? "active" : ""}>
-            <h1>
-              Receive an RSVP in the mail
-              <button id="form-close" onClick={() => setActive(false)}>
-                &times;
-              </button>
-            </h1>
+          <div className={isActive ? "active" : ""} id="rsvp-form">
+            <header>
+              <h1>
+                Receive an RSVP in the mail
+                <button id="form-close" onClick={() => setActive(false)}>
+                  &times;
+                </button>
+              </h1>
+            </header>
 
-            <ul>
-              <li>
-                <strong>Please do not book a hotel yet</strong>. We are blocking
-                out at least one hotel and we will give you all the information
-                you need with plenty of time to make plans.
-              </li>
+            <Form>
+              <ul>
+                <li>
+                  <strong>Please do not book a hotel yet</strong>. We are
+                  blocking out at least one hotel and we will give you all the
+                  information you need with plenty of time to make plans.
+                </li>
 
-              <li>
-                Go ahead and book your flight, though! The airport you should
-                fly to is Puerto Vallarta, or, <strong>PRV</strong>. We will be
-                providing transportation service.
-              </li>
+                <li>
+                  Go ahead and book your flight, though! The airport you should
+                  fly to is Puerto Vallarta, or, <strong>PRV</strong>. We will
+                  be providing transportation service.
+                </li>
 
-              <li>
-                <strong>
-                  We are not asking for gifts, <em>especially</em> from our
-                  guests
-                </strong>
-                &nbsp;who are making such a huge commitment to celebrate with us
-                on our special day. We understand that some may wish to give a
-                gift in their absence; please&nbsp;
-                <a
-                  href="mailto:joe@joesak.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  email Joe
-                </a>{" "}
-                if you wish to contribute toward our wedding and honeymoon
-                costs.
-              </li>
-              <li>
-                Venue space is limited, so please keep your party to two as is
-                possible.
-              </li>
-              <li>
-                No matter how you choose to celebrate with us, either in person
-                or from afar, we seriously cannot thank you enough. We're going
-                to do everything we can to make it an easy, fun, and memorable
-                experience for our guests!
-              </li>
-            </ul>
+                <li>
+                  <strong>
+                    We are not asking for gifts, <em>especially</em> from our
+                    guests
+                  </strong>
+                  &nbsp;who are making such a huge commitment to celebrate with
+                  us on our special day. We understand that some may wish to
+                  give a gift in their absence; please&nbsp;
+                  <a
+                    href="mailto:joe@joesak.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    email Joe
+                  </a>{" "}
+                  if you wish to contribute toward our wedding and honeymoon
+                  costs.
+                </li>
+                <li>
+                  Venue space is limited, so please keep your party to two as is
+                  possible.
+                </li>
+                <li>
+                  No matter how you choose to celebrate with us, either in
+                  person or from afar, we seriously cannot thank you enough.
+                  We're going to do everything we can to make it an easy, fun,
+                  and memorable experience for our guests!
+                </li>
+              </ul>
 
-            <div className="form">
-              <div className="input-group">
-                <RsvpField name="name" label="Name" />
-                <RsvpField name="email" label="Email" />
+              <div className="form">
+                <div className="input-group">
+                  <RsvpField name="name" label="Name" />
+                  <RsvpField name="email" label="Email" />
+                </div>
+
+                <RsvpField
+                  name="howMany"
+                  label="How many expected in your party?"
+                  note="you will have a chance to change this"
+                />
+
+                <RsvpField name="streetAddress" label="Street Address" />
+                <RsvpField name="streetAddress2" label="Street Address 2" />
+
+                <div className="input-group">
+                  <RsvpField name="city" label="City" />
+                  <RsvpField name="state" label="State / Province" />
+                  <RsvpField name="postalCode" label="Postal Code" />
+                </div>
+
+                <RsvpField name="country" label="Country" />
+
+                {renderButton(isSubmitting)}
               </div>
-
-              <RsvpField
-                name="howMany"
-                label="How many expected in your party?"
-                note="you will have a chance to change this"
-              />
-
-              <RsvpField name="streetAddress" label="Street Address" />
-              <RsvpField name="streetAddress2" label="Street Address 2" />
-
-              <div className="input-group">
-                <RsvpField name="city" label="City" />
-                <RsvpField name="state" label="State / Province" />
-                <RsvpField name="postalCode" label="Postal Code" />
-              </div>
-
-              <RsvpField name="country" label="Country" />
-
-              {renderButton(isSubmitting)}
-            </div>
-          </Form>
+            </Form>
+          </div>
         </>
       )}
     </Formik>
